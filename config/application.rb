229ec6b364ab28bca4a09ec213d7861d7255879b
yusuke.saito <yusuke.saito@smartscape.co.jp>
2016-12-14 10:50:26 +0900

    データベース設定をconfig/settings.ymlに移動

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
+
 module RailsRiot
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
